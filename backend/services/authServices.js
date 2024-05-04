23eeda45fd0041a5ee65ed01cf2137c650e0eec7
Christian Hart <christiantaylorhart@gmail.com>
2024-05-04 13:37:07 -0400

    Set up password hashing util, add to register route

--- backend/services/authServices.js
+++ backend/services/authServices.js
@@ -1,13 +1,16 @@
 import { StatusCodes } from 'http-status-codes';
 import UserList from '../daos/models/UserModel.js';
+import { hashPassword } from '../../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await UserList.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const newUser = await UserList.create(req.body);
-  res
-    .status(StatusCodes.CREATED)
-    .json({ msg: 'User successfully created', newUser });
+  res.status(StatusCodes.CREATED).json({ msg: 'User successfully created' });
 };
 export const login = async (req, res) => {
   res.send('Login');
