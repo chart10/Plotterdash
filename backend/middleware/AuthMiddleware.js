dc5c8a21652755c8fd3878891fbf1134425599dc
Christian Hart <christiantaylorhart@gmail.com>
2024-05-05 12:09:54 -0400

    Auth middleware verifies token is valid

--- backend/middleware/AuthMiddleware.js
+++ backend/middleware/AuthMiddleware.js
@@ -1,7 +1,15 @@
+import { verifyJWT } from '../../utils/tokenUtils.js';
 import { UnauthenticatedError } from '../errors/CustomError.js';
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) throw new UnauthenticatedError('Authentication invalid');
-  next();
+  try {
+    const user = verifyJWT(token);
+    console.log(user);
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new UnauthenticatedError('Authentication invalid');
+  }
 };
